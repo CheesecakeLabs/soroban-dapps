0b21edc470c1515ab3eb08003e3c6b4885e2c299
alessandrak <alessakc@gmail.com>
2023-08-30 15:35:06 -0300

    remove unused methods

--- event-watcher/backend/src/db/sqlite.rs
+++ event-watcher/backend/src/db/sqlite.rs
@@ -7,17 +7,6 @@ pub struct SqliteDriver {
 
 use crate::{Pool, Token};
 
-pub struct EventRecord {
-    pub ledger: u32,
-    pub event_id: String,
-    pub contract_id: String,
-    pub topic_1: String,
-    pub topic_2: String,
-    pub topic_3: String,
-    pub topic_4: String,
-    pub xdr_data: String,
-}
-
 pub fn get_connection() -> Result<SqliteDriver> {
     let conn = Connection::open("./database.db").expect("error opening sqlite database file");
     Ok(SqliteDriver { conn })
@@ -71,54 +60,6 @@ impl SqliteDriver {
         )?;
 
         Ok(())
-    }
-
-    pub fn insert_event(&self, event: &EventRecord) -> Result<()> {
-        self.conn.execute(
-            "INSERT INTO rpc_event (
-        ledger, event_id, contract_id, topic_1, topic_2, topic_3, topic_4, xdr_data
-      ) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
-            (
-                event.ledger,
-                &event.event_id,
-                &event.contract_id,
-                &event.topic_1,
-                &event.topic_2,
-                &event.topic_3,
-                &event.topic_4,
-                &event.xdr_data,
-            ),
-        )?;
-
-        Ok(())
-    }
-
-    pub fn get_last_ledger(&self) -> Result<u32> {
-        /*let mut stmt = self.conn.prepare("SELECT max(ledger) FROM rpc_event")?;
-        let query_row = stmt.query_row([], |row: &Row<'_>| -> Row { row });
-
-        let row = match query_row {
-          Ok(r) => r,
-          Err(QueryReturnedNoRows) => return Ok(0), // if no rows, we just return 0 to represent that there's no value
-          Err(e) => return Err(e),
-        };
-
-        match row.get::<usize, u32>(0) {
-          Ok(v) => return Ok(v),
-          Err(e) => return Err(e),
-        }*/
-
-        let ledger =
-            match self
-                .conn
-                .query_row::<u32, _, _>("SELECT max(ledger) FROM rpc_event", [], |row| row.get(0))
-            {
-                Ok(r) => r,
-                Err(rusqlite::Error::QueryReturnedNoRows) => return Ok(0), // if no rows, we just return 0 to represent that there's no value
-                Err(e) => return Err(e),
-            };
-
-        Ok(ledger)
     }
 
     pub fn list_tokens(&self) -> Result<Vec<Token>, rusqlite::Error> {
