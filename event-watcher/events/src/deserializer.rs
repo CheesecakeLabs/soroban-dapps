288a57cd479fe8bd1a701ce9283679157127d329
alessandrak <alessakc@gmail.com>
2023-08-30 10:51:25 -0300

    add account type on deserializer

--- event-watcher/events/src/deserializer.rs
+++ event-watcher/events/src/deserializer.rs
@@ -1,4 +1,6 @@
-use stellar_xdr::next::{ContractEventV0, ScSymbol, ScVal, ScVec};
+use stellar_xdr::next::{
+    AccountId, ContractEventV0, PublicKey, ScAddress, ScSymbol, ScVal, ScVec, Uint256,
+};
 pub enum EventValue {
     String(String),
     Bool(bool),
@@ -33,16 +35,19 @@ impl EventDeserializer {
     pub fn extract_topic(v0: &ContractEventV0, index: usize) -> Option<EventValue> {
         if let Some(topic) = v0.topics.get(index) {
             match topic {
-                ScVal::Symbol(ScSymbol(symbol)) => {
-                    Some(EventValue::String(symbol.to_string().unwrap()))
+                ScVal::Symbol(ScSymbol(value)) => {
+                    Some(EventValue::String(value.to_string().unwrap()))
                 }
                 ScVal::Bool(value) => Some(EventValue::Bool(*value)),
+                ScVal::Address(ScAddress::Account(AccountId(PublicKey::PublicKeyTypeEd25519(
+                    value,
+                )))) => Some(EventValue::String(value.to_string())),
                 _ => None,
             }
         } else {
             None
         }
-    }
+    } //
 
     pub fn extract_data_vec(sc_val: &ScVal, index: usize) -> Option<EventValue> {
         if let ScVal::Vec(Some(ScVec(vec_m))) = sc_val {
