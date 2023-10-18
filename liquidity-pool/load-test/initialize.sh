bcb2f8440d9c9c4230adced6722a7d4fa91f74c8
Alexandre Seemund <alexandreseemund@gmail.com>
2023-10-18 16:19:14 -0300

    create account script

--- liquidity-pool/load-test/initialize.sh
+++ liquidity-pool/load-test/initialize.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
+set -e
+
 source ./config.sh
 
 STYLE='\033[1;37;44m'
 NS='\033[0m' # No Color
+
+if [ "$NETWORK_NAME" == "futurenet" ]; then
+    SOROBAN_RPC_HOST="https://rpc-futurenet.stellar.org:443"
+    SOROBAN_RPC_URL="$SOROBAN_RPC_HOST"
+else # testnet
+    SOROBAN_RPC_HOST="https://soroban-testnet.stellar.org:443"
+    SOROBAN_RPC_URL="$SOROBAN_RPC_HOST"
+fi
+
+case "$NETWORK_NAME" in
+    futurenet)
+    echo "Using Futurenet network with RPC URL: $SOROBAN_RPC_URL"
+    SOROBAN_NETWORK_PASSPHRASE="Test SDF Future Network ; October 2022"
+    ;;
+    testnet)
+    echo "Using Testnet network with RPC URL: $SOROBAN_RPC_URL"
+    SOROBAN_NETWORK_PASSPHRASE="Test SDF Network ; September 2015"
+    ;;  
+    *)
+    echo "Usage: $0 futurenet|testnet [rpc-host]"
+    exit 1
+    ;;
+esac
+
+echo Add the $NETWORK_NAME network to cli client
+soroban config network add \
+  --rpc-url "$SOROBAN_RPC_URL" \
+  --network-passphrase "$SOROBAN_NETWORK_PASSPHRASE" "$NETWORK_NAME"
 
 
 echo -e "\n ${STYLE}BUILDING CONTRACTS...${NS}"
@@ -38,6 +68,9 @@ echo -e "\n ${STYLE}DEPLOYING AMM CONTRACT...${NS}"
 
 echo -e "\n ${STYLE}INITIALIZING AMM CONTRACT...${NS}"
 ./helpers/initialize-amm.sh
+
+echo -e "\n ${STYLE}CREATE ACCOUNTS...${NS}"
+source helpers/create-accounts.sh
 
 echo -e "\n ${STYLE}LOADING USER ACCOUNTS WITH TOKENS...${NS}"
 source helpers/load-accounts.sh
--- liquidity-pool/load-test/tests/liquidity-pool.sh
+++ liquidity-pool/load-test/tests/liquidity-pool.sh
@@ -5,87 +5,61 @@ echo -e "\n ${STYLE} DEPOSITING LIQUIDITY...${NS}"
 INVOKER_SK=""
 CONTRACT_ID=${AMM_CONTRACT_ID}
 
-echo -e "\n Depositing ${DEPOSIT_AMOUNT} units of from user A"
+for file in "${DATA_DIR}/.accounts"/*"-address"
+do
+    USER_PK=$(<"${file}")
+    USER_SK=$(<"${file%-address}-secret")
 
-source helpers/amm/deposit.sh ${USER_A_SK} \
-    ${USER_A_PK} \
-    ${DEPOSIT_AMOUNT} \
-    1 \
-    ${DEPOSIT_AMOUNT} \
-    1 &
+    echo -e "\n Depositing ${DEPOSIT_AMOUNT} units of from account $USER_PK"
 
-echo -e "\n Depositing ${DEPOSIT_AMOUNT} units of from user B"
+    source helpers/amm/deposit.sh ${USER_SK} \
+        ${USER_PK} \
+        ${DEPOSIT_AMOUNT} \
+        1 \
+        ${DEPOSIT_AMOUNT} \
+        1 &
 
-source helpers/amm/deposit.sh ${USER_B_SK} \
-    ${USER_B_PK} \
-    ${DEPOSIT_AMOUNT} \
-    1 \
-    ${DEPOSIT_AMOUNT} \
-    1 &
-
-echo -e "\n Depositing ${DEPOSIT_AMOUNT} units of from user C"
-
-source helpers/amm/deposit.sh ${USER_C_SK} \
-    ${USER_C_PK} \
-    ${DEPOSIT_AMOUNT} \
-    1 \
-    ${DEPOSIT_AMOUNT} \
-    1 &
-
+done
 wait
 
 echo -e "\n ${STYLE} TRIGGERING SWAPS...${NS}"
 
 declare -i swaps_executed=1
 declare -i total_swaps=${TOTAL_SWAPS}
-declare -i random_user=1
 
 while [ ${swaps_executed} -le ${total_swaps} ]
 do
 
-    for ((i = 1; i <= ${PARALLEL_SWAPS}; i++))
+    for file in "${DATA_DIR}/.accounts"/*"-address"
     do
+        USER_PK=$(<"${file}")
+        INVOKER_SK=$(<"${file%-address}-secret")
+
         echo -e "\n  PARALLEL for\n"
         swap_amount=$(( RANDOM % (SWAP_MAX_AMOUNT - SWAP_MIN_AMOUNT + 1) + SWAP_MIN_AMOUNT ))
-        INVOKER_SK=""
-        USER_PK=""
-
-        if (( $random_user > 3 )); then
-            random_user=1
-        fi
-        case $random_user in
-        1)
-            INVOKER_SK=${USER_A_SK}
-            USER_PK=${USER_A_PK}
-            ;;
-        2)
-            INVOKER_SK=${USER_B_SK}
-            USER_PK=${USER_B_PK}
-            ;;
-        3)
-            INVOKER_SK=${USER_C_SK}
-            USER_PK=${USER_C_PK}
-            ;;
-        esac
 
         echo -e "\n  Swap n-${swaps_executed} -> ${USER_PK} is swapping ${swap_amount} units... \n"
         
         source helpers/swap.sh "$USER_PK" "$INVOKER_SK" "$swap_amount" true 9999999999999999 true &
         ((swaps_executed++))
-        ((random_user++))
     done
     wait
+    INVOKER_SK=${ADMIN_SK}
+    FUNCTION_NAME="get_rsrvs"
+    source helpers/invoke.sh \
+        ${CONTRACT_ID} \
+        ${FUNCTION_NAME} \
+        ${INVOKER_SK}
 done
 
-echo -e "\n Withdraw ${SHARE_AMOUNT} units of from user A"
-source helpers/withdraw.sh "$USER_A_PK" "$USER_A_SK" "$SHARE_AMOUNT" 10000000 10000000 &
+for file in "data/.accounts"/*"-address"
+do
+    USER_PK=$(<"${file}")
+    USER_SK=$(<"${file%-address}-secret")
 
-echo -e "\n Withdraw ${SHARE_AMOUNT} units of from user B"
-source helpers/withdraw.sh "$USER_B_PK" "$USER_B_SK" "$SHARE_AMOUNT" 10000000 10000000 &
-
-echo -e "\n Withdraw ${SHARE_AMOUNT} units of from user C"
-source helpers/withdraw.sh "$USER_C_PK" "$USER_C_SK" "$SHARE_AMOUNT" 10000000 10000000 &
-
+    echo -e "\n Withdraw ${SHARE_AMOUNT} units of from user A"
+    source helpers/withdraw.sh "$USER_PK" "$USER_SK" "$SHARE_AMOUNT" 10000000 10000000 &
+done
 wait
 
 echo -e "\n ${STYLE} CHECKING POOL RESERVES...${NS}"
@@ -96,16 +70,5 @@ source helpers/invoke.sh \
     ${CONTRACT_ID} \
     ${FUNCTION_NAME} \
     ${INVOKER_SK}
-
-source helpers/invoke.sh \
-    ${CONTRACT_ID} \
-    ${FUNCTION_NAME} \
-    ${INVOKER_SK}
-
-source helpers/invoke.sh \
-    ${CONTRACT_ID} \
-    ${FUNCTION_NAME} \
-    ${INVOKER_SK}    
-
 
 echo -e "\n Finished"
