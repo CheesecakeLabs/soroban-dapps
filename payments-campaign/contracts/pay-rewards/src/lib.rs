0d7999ec5779298b11e01ae17e014644e9cabdfb
Fabricius Zatti <fazzatti@gmail.com>
2024-07-03 20:21:00 -0300

    feat: payments campaign

---
+++ payments-campaign/contracts/pay-rewards/src/lib.rs
@@ -0,0 +1,6 @@
+#![no_std]
+
+mod admin;
+mod contract;
+mod campaign;
+mod test;   
---
+++ payments-campaign/contracts/pay-rewards/src/test.rs
@@ -0,0 +1,99 @@
+#![cfg(test)]
+
+use super::*;
+use campaign::CampaignStatus;
+use contract::{PaymentRewards, PaymentRewardsClient};
+use soroban_sdk::{  testutils::Address as _, token::{TokenClient, TokenInterface, StellarAssetClient}, Address, Env};
+
+#[test]
+fn test() {
+    let env = Env::default();
+    let asset_admin = Address::generate(&env);
+    let admin = Address::generate(&env);
+    let user_a = Address::generate(&env);
+    let user_b = Address::generate(&env);
+
+
+    let end_time =   env.ledger().sequence()+    100;
+
+    env.mock_all_auths();
+
+    let asset = env.register_stellar_asset_contract(asset_admin.clone());
+    let asset_client_admin = StellarAssetClient::new(&env, &asset);
+
+    let asset_client = TokenClient::new(&env, &asset);
+
+    let contract_id = env.register_contract(None, PaymentRewards);
+
+    let pay_rewards_client = PaymentRewardsClient::new(&env, &contract_id);
+
+    // Mint initial funds to ADMIN
+    asset_client_admin.mint(&admin, &1000);
+    assert_eq!(asset_client.balance(&admin), 1000);
+
+    //Mint initial funds to users
+    asset_client_admin.mint(&user_a, &100);
+    asset_client_admin.mint(&user_b, &100);
+    assert_eq!(asset_client.balance(&user_a), 100);
+    assert_eq!(asset_client.balance(&user_b), 100);
+
+    // Initialize contract but not campaign
+    pay_rewards_client.initialize(&admin, &asset, &end_time, &5, &20, &false);
+    assert_eq!(pay_rewards_client.status() , CampaignStatus::Completed); // considered completed due to it being depleted from balance
+    assert_eq!(pay_rewards_client.balance(), 0);
+    assert_eq!(pay_rewards_client.get_cumulative_quota(&user_a), 0);
+    assert_eq!(pay_rewards_client.check_eligibility(&user_a), true);
+
+    // Add funds to contract
+    pay_rewards_client.add_funds(&admin, &500);
+    assert_eq!(pay_rewards_client.balance(), 500);
+    assert_eq!(asset_client.balance(&admin), 500);
+    assert_eq!(pay_rewards_client.status() , CampaignStatus::Inactive);
+
+
+    // Start campaign
+    pay_rewards_client.start();
+    assert_eq!(pay_rewards_client.status() , CampaignStatus::Active);
+
+    // Send from user_a to user_b - partial quota
+    pay_rewards_client.send(&user_a, &user_b, &10);
+    assert_eq!(pay_rewards_client.get_cumulative_quota(&user_a), 10);
+    assert_eq!(pay_rewards_client.get_cumulative_quota(&user_b), 0);
+    assert_eq!(asset_client.balance(&user_a), 90);
+    assert_eq!(asset_client.balance(&user_b), 110);
+
+    //send from user_a to user_b - full quota
+    pay_rewards_client.send(&user_a, &user_b, &15);
+    assert_eq!(pay_rewards_client.get_cumulative_quota(&user_a), 20);
+    assert_eq!(pay_rewards_client.get_cumulative_quota(&user_b), 0);
+    assert_eq!(asset_client.balance(&user_a), 75+5);
+    assert_eq!(asset_client.balance(&user_b), 125);
+    assert_eq!(pay_rewards_client.balance(), 500-5);
+    assert_eq!(pay_rewards_client.check_eligibility(&user_a), false);
+    assert_eq!(pay_rewards_client.check_eligibility(&user_b), true);
+
+    // Pause campaign
+    pay_rewards_client.pause();
+    assert_eq!(pay_rewards_client.status() , CampaignStatus::Inactive);
+    assert_eq!(pay_rewards_client.check_eligibility(&user_a), false);
+    assert_eq!(pay_rewards_client.check_eligibility(&user_b), true);
+
+    // send from user_b to user_a - campaign paused
+    pay_rewards_client.send(&user_b, &user_a, &10);
+    assert_eq!(pay_rewards_client.get_cumulative_quota(&user_b), 0);
+    assert_eq!(asset_client.balance(&user_b), 115);
+
+    //semd more from user_b to user_a - campaign paused
+    pay_rewards_client.send(&user_b, &user_a, &50);
+    assert_eq!(pay_rewards_client.get_cumulative_quota(&user_b), 0);
+    assert_eq!(asset_client.balance(&user_b), 65);
+
+    // Start campaign again
+    pay_rewards_client.start();
+
+    // send from user_b to user_a - campaign active
+    pay_rewards_client.send(&user_b, &user_a, &50);
+    assert_eq!(pay_rewards_client.get_cumulative_quota(&user_b), 20);
+    assert_eq!(asset_client.balance(&user_b), 20);
+
+}
